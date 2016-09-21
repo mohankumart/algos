12d120da4d114ef0230064d03ba55804524f381e
tmk_mohankumar@yahoo.com <tmk_mohankumar@yahoo.com>
2016-09-21 20:24:07 +0530

    for python syntax verifications

---
+++ codingInterview/utils.py
@@ -0,0 +1,15 @@
+bit_number = 0
+
+bit_number = bit_number | 1 << 2
+print(bit_number)
+print(bin(bit_number))
+
+bit_number = bit_number | 1 << 3
+print(bit_number)
+print(bin(bit_number))
+
+bit_number = bit_number | 1 << 4
+print(bit_number)
+print(bin(bit_number))
+
+
