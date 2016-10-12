60cecdac959628563a229da7b60a44a7e84ab40b
tmk_mohankumar@yahoo.com <tmk_mohankumar@yahoo.com>
2016-10-12 20:34:04 +0530

    oneway improvements

--- codingInterview/oneaway.py
+++ codingInterview/oneaway.py
@@ -1,11 +1,32 @@
 #oneway program
 
-def oneaway():
-    pass
+def oneaway(str1, str2):
+    if len(str1) == len(str2): #one char replace from str2
+        return oneCharReplaced(str1, str2)
+    elif len(str1) + 1 == len(str2): #one extra char added in str2
+        return oneCharDeleted(str1, str2)
+    elif len(str1) - 1 == len(str2): #one char removed from str2
+        return oneCharDeleted(str2, str1)
 
+def oneCharReplaced(str1, str2):
+    for i in range(len(str1)):
+        if str1[i] != str2[i]:
+            return False
+    return True
 
-def oneCharReplaced():
-    pass
-
-def oneDeleted():
-    pass
+#str2 > str1 
+def oneCharDeleted(str1, str2):
+    index1 = 0
+    index2 = 0
+    while index1 <= len(str1)-1 and index2 <= len(str2)-1:
+        if str1[index1] != str2[index2]:
+            if index1 != index2:
+                return False
+            index2 += 1
+        else:
+            index1 += 1
+            index2 += 1
+    return True
+str1 = raw_input('Enter str1: ')
+str2 = raw_input('Enter str2: ')
+print(oneaway(str1, str2))
