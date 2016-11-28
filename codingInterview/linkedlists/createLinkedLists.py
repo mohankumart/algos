f9e08cf3b9a3bd1738af47952cdcc2913ada0222
tmk_mohankumar@yahoo.com <tmk_mohankumar@yahoo.com>
2016-11-28 12:09:39 +0530

    linked list with no space code

--- codingInterview/linkedlists/createLinkedLists.py
+++ codingInterview/linkedlists/createLinkedLists.py
@@ -54,13 +54,50 @@ class LinkedList:
             previous = previous.next_node
         previous.next_node = current.next_node
         current.next_node = None
+    def remove_duplicates(self):
+        #temp list to store data elements adnd check their existence
+        dup_list = list()
+        previous = self.head
+        current = self.head
+        #loop over linked list
+        while current:
+            found = False
+            #import pdb;pdb.set_trace()
+            for i in range(len(dup_list)):
+                #print("Mohan")
+                if current.data == dup_list[i]:
+                    found = True
+                    break
+            if not found:
+                dup_list.append(current.data)
+                previous = current
+                current = current.next_node
+            else:
+                previous.next_node = current.next_node
+                current = previous.next_node
 
-
-
+    def remove_duplicates_nospace(self):
+        current = self.head
+        while current:
+            runner = current.next_node
+            previous_runner = current
+            while runner:
+                if runner.data == current.data:
+                    previous_runner.next_node = runner.next_node
+                    runner = previous_runner.next_node
+                else:
+                    previous_runner = runner
+                    runner = runner.next_node
+            current = current.next_node
 
 my_list = LinkedList()
-for i in range(10):
-    my_list.insert_node(i)
-my_list.print_list()
 
+for i in range(5):
+    i = raw_input("Enter data:" )
+    my_list.insert_node(int(i))
+
+my_list.print_list()
+#my_list.remove_duplicates()
+my_list.remove_duplicates_nospace();
+my_list.print_list()
 
